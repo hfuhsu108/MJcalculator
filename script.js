e85e22418a965f461f28697beda101da6b9791de
hfuhsu108 <66896423+hfuhsu108@users.noreply.github.com>
2023-01-22 14:58:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// 儲存輸入的數字和時間
+var numbers = [];
+
+// 紀錄按鈕
+document.getElementById("recordBtn").addEventListener("click", function(){
+  //取得當前時間
+  var now = new Date();
+  //取得使用者輸入的數字
+  var inputNum = document.getElementById("inputNumber").value;
+  //將使用者輸入的數字和時間存入陣列中
+  numbers.push({number: inputNum, time: now});
+  // 將資料新增至表格中
+  var recordTable = document.getElementById("recordTable");
+  var newRow = recordTable.insertRow(-1);
+  var newCell1 = newRow.insertCell(0);
+  var newCell2 = newRow.insertCell(1);
+  newCell1.innerHTML = inputNum;
+  newCell2.innerHTML = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
+});
+
+// 加總按鈕
+document.getElementById("addBtn").addEventListener("click", function(){
+  //計算陣列中的總和
+  var sum = numbers.reduce(function(acc, cur) {
+    return acc + parseInt(cur.number);
+  }, 0);
+  //顯示總和
+  document.getElementById("result").innerHTML = "總和: " + sum;
+});
+
+// 清除按鈕
+document.getElementById("clearBtn").addEventListener("click", function(){
+  //清空陣列
+  numbers = [];
+  //清空輸入框
+  document.getElementById("inputNumber").value = "";
+  //清空總和
+  document.getElementById("result").innerHTML = "";
+  //清除表格內容
+  var recordTable = document.getElementById("recordTable");
+  var rows = recordTable.rows;
+  var rowCount = rows.length;
+  for (var i = 1; i < rowCount; i++) {
+    recordTable.deleteRow(1);
+  }
+});
